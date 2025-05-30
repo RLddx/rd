95eb6695d6f4d44084c44f5ea99d41acaea9c766
RLddx <secundaria1814d@gmail.com>
2025-05-30 10:38:55 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 REMOVERalert("Olá curioso, seja bem vindo ao site do QUIZ DO RD!")
 
 botaoIniciar = document.querySelector(".iniciar")
+
+botaoIniciar.addEventListener("click", iniciarJogo)
+
+function iniciarJogo() {
+  telaInicial = document.querySelector(".tela-inicial")
+  telaInicial.style.display = "none"// Tab to edit
+}
